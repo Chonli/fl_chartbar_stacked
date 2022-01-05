68f34773cb608a2631bf1a7f8a3c480280dac03b
Chonli <Chonli@users.noreply.github.com>
2022-01-05 21:47:12 +0100

    implement stacked bar

--- lib/main.dart
+++ lib/main.dart
@@ -13,103 +13,190 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  static const datas = <_ChartData>[
+    _ChartData("12/02/2018", 25, 25),
+    _ChartData("22/06/2018", 5, 20),
+    _ChartData("04/09/2018", 25, 5),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Chart Bar'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              SizedBox(
+                height: 300,
+                child: ChartBarView(
+                  datas: datas,
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _ChartData {
+  const _ChartData(
+    this.date,
+    this.value,
+    this.value2,
+  );
+
+  final String date;
+  final double value;
+  final double value2;
+}
+
+const topPad = 10.0;
+const bottomPad = 30.0;
+const rightPad = 25.0;
+
+class ChartBarView extends StatelessWidget {
+  const ChartBarView({Key? key, required this.datas}) : super(key: key);
+
+  final List<_ChartData> datas;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        final canvasHeight = constraints.maxHeight - (topPad + bottomPad);
+        final canvasWidth = constraints.maxWidth - rightPad;
+        final maxData = datas
+            .reduce((a, b) => a.value + a.value2 > b.value + b.value2 ? a : b);
+        final maxValue = maxData.value + maxData.value2 + 10;
+        final ratioValuePx = canvasHeight / maxValue;
+        final barPad = canvasWidth / (datas.length + 1);
+        print('h=$canvasHeight, w=$canvasWidth, b=$barPad');
+
+        return Container(
+          color: Colors.grey.shade100,
+          child: Stack(
+            children: [
+              // Y axis
+              for (int i = 0; i < 4; i++) ...[
+                Positioned(
+                  top: topPad + i * canvasHeight / 4,
+                  width: canvasWidth,
+                  child: const Divider(
+                    height: 0,
+                    endIndent: rightPad,
+                    color: Colors.black54,
+                  ),
+                ),
+                Positioned(
+                  top: i * canvasHeight / 4,
+                  right: 0,
+                  child: Text('${maxValue - i * maxValue / 4}%'),
+                ),
+              ],
+
+              // X axis
+              Positioned(
+                bottom: bottomPad - 2,
+                width: canvasWidth,
+                child: const Divider(
+                  height: 0,
+                  thickness: 2,
+                  endIndent: rightPad,
+                  color: Colors.black54,
+                ),
+              ),
+
+              // Bar
+              for (int i = 0; i < datas.length; i++) ...[
+                _ChartBar(
+                  data: datas[i],
+                  leftPad: barPad * (i + 1),
+                  ratioValuePx: ratioValuePx,
+                ),
+              ]
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ChartBar extends StatelessWidget {
+  const _ChartBar({
+    Key? key,
+    required this.data,
+    required this.leftPad,
+    required this.ratioValuePx,
+  }) : super(key: key);
+
+  final _ChartData data;
+  final double leftPad;
+  final double ratioValuePx;
+
+  @override
+  Widget build(BuildContext context) {
+    const width = 30.0;
+    final leftPos = leftPad - width / 2;
+    final stackedPosValue2 = data.value2 * ratioValuePx;
+    return Stack(
+      children: [
+        Positioned(
+          left: leftPos,
+          bottom: bottomPad,
+          width: width,
+          height: stackedPosValue2,
+          child: Container(
+            color: Colors.green,
+          ),
+        ),
+        Positioned(
+          left: leftPos,
+          bottom: bottomPad + stackedPosValue2,
+          width: width,
+          height: data.value * ratioValuePx,
+          child: Container(
+            color: Colors.greenAccent,
+          ),
+        ),
+        Positioned(
+          left: leftPad,
+          bottom: bottomPad - 5,
+          height: 5,
+          child: const VerticalDivider(
+            width: 0,
+            thickness: 2,
+            color: Colors.black54,
+          ),
+        ),
+        Positioned(
+          left: leftPad - 25,
+          bottom: 10,
+          child: Text(
+            data.date,
+            style: const TextStyle(fontSize: 10),
+          ),
+        ),
+      ],
     );
   }
 }
